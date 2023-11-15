30fa43fcd8e900ffc529664796b74431bf701797
damonleaf29 <dale3791@colorado.edu>
2023-11-15 15:31:45 -0700

    basic start page w/ edits. TODO: make connected pages

--- BlackjackGUI/src/main/java/com/example/blackjackgui/HelloApplication.java
+++ BlackjackGUI/src/main/java/com/example/blackjackgui/HelloApplication.java
@@ -2,7 +2,9 @@ package com.example.blackjackgui;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,10 +12,18 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+//        stage.setTitle("Hello!");
+//        stage.setScene(scene);
+//        stage.show();
+        //Setup 2
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        Image icon = new Image("https://static.thenounproject.com/png/15599-200.png");
+        //Image icon = new Image("https://static.thenounproject.com/png/15599-200.png");
+        stage.getIcons().add(icon);
+        stage.setTitle("BlackjackGUI");
+        stage.setScene(new Scene(root));
         stage.show();
     }
 

b6ef21917322d411ba11e4f2d012086bf3f356d6
damonleaf29 <dale3791@colorado.edu>
2023-11-15 20:54:45 -0700

    Completed first scene switch.

--- BlackjackGUI/src/main/java/com/example/blackjackgui/NewGamePageController.java
+++ BlackjackGUI/src/main/java/com/example/blackjackgui/NewGamePageController.java
@@ -3,9 +3,11 @@ package com.example.blackjackgui;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
@@ -14,18 +16,11 @@ import java.net.URL;
 import java.util.Objects;
 import java.util.ResourceBundle;
 
-public class SceneController {
+
+public class NewGamePageController implements Initializable{
     private Stage stage;
     private Scene scene;
     private Parent root;
-
-    public void switchToNewGamePage(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("NewGamePage.fxml")));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
     public void switchToStartingPage(ActionEvent event) throws IOException {
         root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("StartingPage.fxml")));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
@@ -34,15 +29,18 @@ public class SceneController {
         stage.show();
     }
 
-    @FXML
-    private Label welcomeText;
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ChoiceBox<Integer> deckNumberChoice;
+    private Integer[] deckOptions = {1, 2, 3, 4, 5};
+    @FXML
+    private ChoiceBox<Integer> npcNumberChoice;
+    private Integer[] npcOptions = {0,1, 2, 3, 4}; //Up to 4 possible NPCs at the table
+    @Override //Populates the Choice box for the # of decks the player wants
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        deckNumberChoice.getItems().addAll(deckOptions);
+        npcNumberChoice.getItems().addAll(npcOptions);
+        //TODO MAKE ANOTHER SCENE CONTROLLER FOR SCENE 1. do this so we can link pg 1 and 2
     }
-
-
-
-    //startAmountTxtField
 }

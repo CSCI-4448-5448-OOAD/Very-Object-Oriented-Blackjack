4d14f7a63713f45c76ac937254864cf86e7d4696
damonleaf29 <dale3791@colorado.edu>
2023-11-22 15:05:25 -0700

    Cards can be dealt to players hands. Displays all cards at once atm

--- BlackjackGUI/src/main/java/com/example/blackjackgui/GameTablePageController.java
+++ BlackjackGUI/src/main/java/com/example/blackjackgui/GameTablePageController.java
@@ -11,6 +11,8 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -52,21 +54,40 @@ public class GameTablePageController {
     public void displayDeckNumber(Integer deckNumber){
         deckNumberLabel.setText(Integer.toString(deckNumber) + "- Deck Shoe");
     }
+
     @FXML
     Label NPC1Label;
     @FXML
+    AnchorPane NPC1CardSlot1;
+    @FXML
+    AnchorPane NPC1CardSlot2;
+
+    @FXML
     Label NPC2Label;
+    @FXML
+    AnchorPane NPC2CardSlot1;
+    @FXML
+    AnchorPane NPC2CardSlot2;
     @FXML
     Label NPC3Label;
     @FXML
+    AnchorPane NPC3CardSlot1;
+    @FXML
+    AnchorPane NPC3CardSlot2;
+    @FXML
     Label NPC4Label;
-
+    @FXML
+    AnchorPane NPC4CardSlot1;
+    @FXML
+    AnchorPane NPC4CardSlot2;
+    List<AnchorPane> CardSlotList = new ArrayList<AnchorPane>();
+    //Populating this list so the game can more easily know where to deal cards
     public void displayNPCs(Integer npcNumber){
-        List<Label> NPCLabelList = new ArrayList<Label>();
-        NPCLabelList.add(NPC1Label);
-        NPCLabelList.add(NPC2Label);
-        NPCLabelList.add(NPC3Label);
-        NPCLabelList.add(NPC4Label);
+//        List<Label> NPCLabelList = new ArrayList<Label>();
+//        NPCLabelList.add(NPC1Label);
+//        NPCLabelList.add(NPC2Label);
+//        NPCLabelList.add(NPC3Label);
+//        NPCLabelList.add(NPC4Label);
         switch(npcNumber) {
             case 0:
                 // No Game Players
@@ -76,24 +97,52 @@ public class GameTablePageController {
                 NPC2Label.setText(null);
                 NPC3Label.setText(null);
                 NPC4Label.setText(null);
+                CardSlotList.add(p1CardSlot1);
+                CardSlotList.add(NPC1CardSlot1);
+                CardSlotList.add(p1CardSlot2);
+                CardSlotList.add(NPC1CardSlot2);
                 break;
             case 2:
                 NPC1Label.setText("NPC1");
                 NPC2Label.setText("NPC2");
                 NPC3Label.setText(null);
                 NPC4Label.setText(null);
+                CardSlotList.add(p1CardSlot1);
+                CardSlotList.add(NPC1CardSlot1);
+                CardSlotList.add(NPC2CardSlot1);
+                CardSlotList.add(p1CardSlot2);
+                CardSlotList.add(NPC1CardSlot2);
+                CardSlotList.add(NPC2CardSlot2);
                 break;
             case 3:
                 NPC1Label.setText("NPC1");
                 NPC2Label.setText("NPC2");
                 NPC3Label.setText("NPC3");
                 NPC4Label.setText(null);
+                CardSlotList.add(p1CardSlot1);
+                CardSlotList.add(NPC1CardSlot1);
+                CardSlotList.add(NPC2CardSlot1);
+                CardSlotList.add(NPC3CardSlot1);
+                CardSlotList.add(p1CardSlot2);
+                CardSlotList.add(NPC1CardSlot2);
+                CardSlotList.add(NPC2CardSlot2);
+                CardSlotList.add(NPC3CardSlot2);
                 break;
             case 4:
                 NPC1Label.setText("NPC1");
                 NPC2Label.setText("NPC2");
                 NPC3Label.setText("NPC3");
                 NPC4Label.setText("NPC4");
+                CardSlotList.add(p1CardSlot1);
+                CardSlotList.add(NPC1CardSlot1);
+                CardSlotList.add(NPC2CardSlot1);
+                CardSlotList.add(NPC3CardSlot1);
+                CardSlotList.add(NPC4CardSlot1);
+                CardSlotList.add(p1CardSlot2);
+                CardSlotList.add(NPC1CardSlot2);
+                CardSlotList.add(NPC2CardSlot2);
+                CardSlotList.add(NPC3CardSlot2);
+                CardSlotList.add(NPC4CardSlot2);
                 break;
 
         }
@@ -113,10 +162,14 @@ public class GameTablePageController {
     private AnchorPane p1CardSlot2;
     @FXML
     private AnchorPane p1CardSlot3;
+
+
+    //TODO Need to start making a function to encapsulate the glackjack logic
+    //todo START WITH finding out how many players there are and dealing two cards to each
     public int counter = 0;
     public void createCard(ActionEvent event){
         //♥♦♠♣ Suit resource
-        Label sampleCard = new Label("7♦");
+        Label sampleCard = new Label("7♠");
         //Label sampleCard = new Label("7&spades");
 
         sampleCard.getStylesheets().add(getClass().getResource("CardStyling.css").toExternalForm());
@@ -131,6 +184,28 @@ public class GameTablePageController {
             p1CardSlot2.getChildren().add(sampleCard);
         }
         counter++;
+
+    }
+
+    public void minBet(ActionEvent event) throws InterruptedException {
+        //TODO NOTE: The cards here are just for visuals. Real cards will b held w/in their
+        //todo own classes.
+        for(AnchorPane cardSlot : CardSlotList){
+            //♥♦♠♣ Suit resource
+            dealSingleCard(cardSlot);//can change this to take whatever value is generated from the card classes
+            Thread.sleep(500); //TODO Doesn't delay correctly. Waits and then displays all
+        }
+    }
+    public void dealSingleCard(AnchorPane cardSlot) throws InterruptedException {
+        String[] cardNums = new String[]{"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+        String[] cardSuits = new String[]{"♥","♦","♠","♣"};
+        Random rand = new Random();
+        int numIdx = rand.nextInt(13);
+        int suitIdx = rand.nextInt(4);
+        Label sampleCard = new Label(cardNums[numIdx]+cardSuits[suitIdx]);
+        sampleCard.getStylesheets().add(getClass().getResource("CardStyling.css").toExternalForm());
+        sampleCard.getStyleClass().add("sampleCard");
+        cardSlot.getChildren().add(sampleCard);
 
     }
 }
